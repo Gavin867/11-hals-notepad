45713c4b14d66e28392b0c3ec9b93c70806540b0
Gavin Calkins <gavin.calkins@gmail.com>
2020-12-30 13:44:13 -0800

    added routes code and get api notes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,40 @@
+const { response } = require("express");
+const fs = require("fs");
+
+// Alternative way to set up express router
+const router = require("express").Router();
+
+// File path
+// Get "/api/note" responds with all notes from the database - db.json
+router.get("/notes", function (req, res) {
+
+    fs.readfile ("/db/db.json", "utf8", (err, response) => {
+
+        if (err) throw err;
+
+        let notepad;
+
+        try {
+
+            notepad = [].concat(JSON.parse(response));
+        
+        } catch (err) {
+       
+            notepad = [];
+        }
+
+        console.log("Reading notes", notepad);
+
+        res.json(notepad);
+
+    });
+
+
+});
+
+// post
+
+
+// delete
+
+module.exports = router;
